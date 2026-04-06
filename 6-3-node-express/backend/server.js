f0637bba5fe7ef0c974fdaaac00fbb2f772fa463
Ghala <you@example.com>
2026-04-06 10:38:26 +0300

    todo6

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -15,9 +15,14 @@ import morgan from "morgan";
 app.use(morgan("dev"));
 
 // TODO 6.1: Create root route "/"
-
+app.get("/", (req, res) => {
+    res.send("Welcome to the Quotes API");
+});
 
 // TODO 6.2: Create "/api/quote" route
-
+app.get("/api/quote", (req, res) => {
+    const quote = getRandomQuote();
+    res.json({ quote });
+});
 
 // TODO 7: Start server using app.listen
